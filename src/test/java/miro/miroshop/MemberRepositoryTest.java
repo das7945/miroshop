6cbc2e684e2ad15e408555c8d6e481f21d9e0f89
das7945 <das7945@naver.com>
2023-09-07 05:00:12 +0900

    유틸성 패키지 추가 에러잡는중.....

--- src/test/java/miro/miroshop/MemberRepositoryTest.java
+++ src/test/java/miro/miroshop/MemberRepositoryTest.java
@@ -2,14 +2,16 @@ package miro.miroshop;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-
-@RunWith(SpringRunner.class)
+// @RunWith(SpringRunner.class)은 JUnit 4의 어노테이션이므로
+// 하단의 @ExtendWith(SpringExtension.class)교체함.
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MemberRepositoryTest {
 
@@ -17,7 +19,9 @@ public class MemberRepositoryTest {
 
   @Test
   @Transactional
-  //  @Rollback(false)
+  @Rollback(false)
+  //  테스트에서 트랜잭션을 사용하면 커밋을 안하는게 기본@Rollback(true)이므로
+  //  @Rollback(false) 추가 해야 DB에서 확인할 수 있음.
   public void testMember() throws Exception {
     Member member = new Member();
     member.setUsername("memberA");
@@ -27,5 +31,7 @@ public class MemberRepositoryTest {
 
     Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
     Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    Assertions.assertThat(findMember).isEqualTo(member);
+    //    System.out.println(findMember == member);
   }
 }
